ea6a0878b6b3818f583277791aa499b676ec2b3e
unknown <nagesh1417@gmail.com>
2018-03-17 19:40:46 +0530

    first commit

---
+++ src/spackage1/SLearning1.java
@@ -0,0 +1,17 @@
+package spackage1;
+
+import org.apache.xpath.operations.String;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SLearning1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new FirefoxDriver();
+SLearning1 sl1=new SLearning1();
+driver.get("www.google.com");
+//driver.navigate().to("www.google.com");
+	}
+
+}
